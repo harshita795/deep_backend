4a57e7ff0a27f68b77c41fd109edf9d62e6636d0
harshita795 <yadavharshita261303@gmail.com>
2026-08-20 00:04:42 +0530

    feat: add Python loop control and iteration

---
+++ 1_python/8_loops.py
@@ -0,0 +1,51 @@
+# Python Loops
+# Concepts practiced:
+# - for loops
+
+# For loop
+for i in range(1, 10):
+  print(i)
+
+# "end" parameter in print function to print on same line.
+for i in range(0, 30):
+  print(i, end=" ")
+
+# Unpacking range function using *, this also helps printing on the same line.
+print("\n", *range(1,8)) # \n moves the output to a new line
+
+# Using variable as end value in range function
+end_value = 51
+
+for i in range(1, end_value):
+  print(i, end=" ")
+
+print() # used here for new line
+
+#! range() is used to generate sequences of numbers.
+
+# range() also has optional 3rd parameter called "step"
+# can also use a negative step to count backwards
+for i in range(10, 1, -1):
+  print(i, end=" ")
+
+print()
+
+# Finding the sum of numbers using an in-place variable.
+def sum_of_nums(start, end):
+  total = 0
+  for i in range(start, end):
+    total += i
+  return total
+
+print(sum_of_nums(2, 8))
+
+# Finding the sum of even numbers using step parameter.
+def sum_of_evens(end):
+  total = 0
+  for i in range(0, end, 2):
+    total += i
+  return total
+
+print(sum_of_evens(8))
+
+
