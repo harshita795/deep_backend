fe8066b98dd395405746589d929f2e5726892400
harshita795 <yadavharshita261303@gmail.com>
2026-08-21 21:49:44 +0530

    feat: add list counting, search, and comparison

--- 1_python/lists.py
+++ 1_python/lists.py
@@ -48,3 +48,48 @@ removed_number = numbers.pop(2)
 
 print(removed_number)
 print(numbers)
+
+# Counting the items in a list
+
+apple_count = 0
+orange_count = 0
+banana_count = 0
+
+items = ["apple", "banana", "orange", "apple", "banana"]
+for i in range(0, len(items)):
+  if items[i] == "apple":
+    apple_count += 1
+  elif items[i] == "banana":
+    banana_count += 1
+  elif items[i] == "orange":
+    orange_count +=1
+
+print(f"apple_count: {apple_count}, orange_count: {orange_count}, banana_count: {banana_count}")
+
+# Find an item in a list
+# no-index syntax
+found = False
+
+for item in items:
+  if item == "orange": 
+    found = True
+print(found)
+
+# Find the increase
+old_ages = [1, 42, 43, 53, 12, 3, 32, 34, 54, 32, 43]
+new_ages = [1, 42, 45, 54, 12, 3, 32, 38, 54, 32, 42]
+
+for i in range(0, len(old_ages)):
+  if old_ages[i] < new_ages[i]:
+    print(i, end=" ")
+
+print()
+
+# Find Max
+nums = [1,4,5,8,2,3]
+max_so_far = float("-inf")
+for num in nums:
+  if num > max_so_far:
+    max_so_far = num
+print(max_so_far)
+        
