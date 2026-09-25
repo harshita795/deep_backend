f90b21cf8aa41d0554661bbfdf1b42869acfac4c
Harshita <yadavharshita261303@gmail.com>
2026-09-25 14:50:06 +0100

    Implement frame rate limiting and delta time tracking

--- 6_Asteroids_project/main.py
+++ 6_Asteroids_project/main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
         log_state()
 
@@ -20,6 +23,7 @@ def main():
 
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
